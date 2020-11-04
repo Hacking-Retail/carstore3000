7b8667afde885fcfd438109b9f279eab90ed444d
louis laporte <louis.laporte12@gmail.com>
2020-11-04 18:04:42 +0100

    api: create main routes

--- src/carstore3000/api.py
+++ src/carstore3000/api.py
@@ -1,6 +1,19 @@
 """Carstore3000 API"""
 from flask import Flask
-from carstore3000.extensions import db, migrate
+
+from carstore3000.extensions import api, db, migrate, ma, auth
+from carstore3000.views.cars import CarsRoutes
+from carstore3000.views.users import UsersRoutes
+from carstore3000.models.users import UserModel
+
+
+@auth.verify_password
+def verify_password(username, password):
+    username = Customer.query.filter_by(username=username).first()
+    if not user or not user.verify_password(password):
+        return False
+    g.username = username
+    return True
 
 
 app = Flask(__name__)
@@ -12,6 +25,10 @@ for k, v in app.config.items():
 
 db.init_app(app)
 migrate.init_app(app)
+ma.init_app(app)
+api.add_resource(CarsRoutes, '/api/v1/cars')
+api.add_resource(UsersRoutes, '/api/v1/users')
+api.init_app(app)
 
 
 if __name__ == '__main__':
