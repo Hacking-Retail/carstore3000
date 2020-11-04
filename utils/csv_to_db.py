df07f2598f3788f167b4bf28c9950b97067a699c
louis laporte <louis.laporte12@gmail.com>
2020-11-04 13:55:22 +0100

    utils: add script for cars database update

---
+++ utils/csv_to_db.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import os
+
+import pandas as pd
+from sqlalchemy import create_engine
+
+script_path = os.path.dirname(os.path.abspath(__file__))
+
+
+def upsert_db(data_path=f"{script_path}/../data/cars.csv"):
+    df = pd.read_csv(data_path)
+    engine = create_engine("postgresql://mpa:maprochaineauto@carstore3000_db_1/carstore3000")
+    df.to_sql('cars', engine, index=False)
+
+
+if __name__ == '__main__':
+    print("Updating data ...")
+    upsert_db()
