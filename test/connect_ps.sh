079a773f9ba3460738a176bb4c2e429db9c6df97
louis laporte <louis.laporte12@gmail.com>
2020-11-04 13:22:01 +0100

    test: remove unused variable

--- test/connect_ps.sh
+++ test/connect_ps.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 readonly CONTAINER_NAME="carstore3000_db_1"
-readonly IP="docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $CONTAINER_NAME"
 
 docker exec -it "$CONTAINER_NAME" psql -U mpa carstore3000
