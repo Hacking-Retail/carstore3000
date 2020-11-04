23696ee63c3c8cc48c261a932bfd1495e0d411a6
louis laporte <louis.laporte12@gmail.com>
2020-11-04 18:04:01 +0100

    api: add extensions for basic auth and REST API

--- src/carstore3000/extensions.py
+++ src/carstore3000/extensions.py
@@ -1,6 +1,12 @@
+from flask_httpauth import HTTPBasicAuth
+from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
+from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 
 
+api = Api()
+auth = HTTPBasicAuth()
 db = SQLAlchemy()
 migrate = Migrate()
+ma = Marshmallow()
